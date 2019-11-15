2fac96385681c7312c9b6754372b2363cb1de304
Satyakama Paul <satyakama.paul@gmail.com>
2019-11-15 05:58:21 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1A0pFEpiJ33-NUZm1_cJekTLCWc1klIKp'
 export_file_name = 'final_resnet_model.pkl'
 
-classes = ['type_1', 'type_2', 'type_3']
+classes = ['Biriyani', 'Friedrice', 'Pulao']
 path = Path(__file__).parent
 
 app = Starlette()
